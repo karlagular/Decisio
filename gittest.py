b1ff2f3b93bf116bd73ef29edc5c0c57fab0a06c
Karla Gular <st182062@uni-stuttgart.de>
2023-10-27 15:53:35 +0200

    updt

---
+++ gittest.py
@@ -0,0 +1,31 @@
+
+from docx.text.paragraph import Paragraph
+from docx.document import Document
+from docx.table import _Cell, Table
+from docx.oxml.text.paragraph import CT_P
+from docx.oxml.table import CT_Tbl
+import docx
+
+
+
+def iter_block_items(parent):
+    if isinstance(parent, Document):
+        parent_elm = parent.element.body
+    elif isinstance(parent, _Cell):
+        parent_elm = parent._tc
+    else:
+        raise ValueError("something's not right")
+
+    for child in parent_elm.iterchildren():
+        if isinstance(child, CT_P):
+            yield Paragraph(child, parent)
+        elif isinstance(child, CT_Tbl):
+            table = Table(child, parent)
+            for row in table.rows:
+                for cell in row.cells:
+                    yield from iter_block_items(cell)
+                    
+#doc = docx.Document('word.docx')
+doc = Document("Projectreports/22-1297_Tobias-Nimz.docx")
+for block in iter_block_items(doc):
+	print(block.text)
